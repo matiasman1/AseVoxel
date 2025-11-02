1f770d9b1467a6446500a2aa3a9e59b15e5c5433
matias <matias.rezinovsky@gmail.com>
2025-11-02 19:30:29 -0300

    Forward shader stack configuration to renderer in updatePreview function

--- core/viewer_core.lua
+++ core/viewer_core.lua
@@ -219,6 +219,8 @@ function viewerCore.updatePreview(dlg, params, controlsDialog, callback)
       rotationMatrix = params.rotationMatrix 
         or (dialogueManager and dialogueManager.currentRotationMatrix),
       fxStack = params.fxStack,
+      -- NEW: Forward shader stack configuration to renderer
+      shaderStack = params.shaderStack,
       -- Forward mesh-mode toggle so previewRenderer / native can choose mesh pipeline
       mesh = params.mesh or params.meshMode,
       meshMode = params.meshMode or params.mesh,
